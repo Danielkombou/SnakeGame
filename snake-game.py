c7ad3077a2207ce529d099620fa3bc0ffaf0ddf6
shanelle-hub <njingtishanelle@gmail.com>
2025-01-23 12:53:01 +0100

    created display grid screen

--- snake-game.py
+++ snake-game.py
@@ -4,21 +4,33 @@ import time
 
 pygame.init()
 # screen settings
-screen_width = 255
-screen_height = 255
+screen_width = 300
+screen_height = 300
 screen = pygame.display.set_mode((screen_width, screen_height))
 
-color =(255,255,255)
+color = (255, 255, 255)
 clock = pygame.time.Clock()
 
+def createGrid():
+    screen.fill((0, 0, 0))
+    # create grids
+    for row in range(15):
+     for col in range(15):
+           x = row * 20
+           y = col * 20
+           pygame.draw.rect(screen, color, pygame.Rect(x, y, 20, 20), 2)
+
+
+
+# Main Game loop
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-            
-    for i in range(4):
-        pygame.draw.rect(screen,color , pygame.Rect(0,0, 0, 10),  2)
-        
-        pygame.display.flip()
+
+    createGrid()
+
+    pygame.display.flip()
+
     pygame.display.update()
-    clock.tick(60)  
+    clock.tick(60)
