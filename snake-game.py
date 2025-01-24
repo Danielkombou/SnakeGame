1e307feccbbd786bf0da6bf780a78ec608b4fc37
Grace <neguekwahammaelgrace@gmail.com>
2025-01-24 08:49:21 +0100

    display score

--- snake-game.py
+++ snake-game.py
@@ -13,14 +13,14 @@ color =(255,255,255)
 clock = pygame.time.Clock()
 
 # snake_position
-snake_position = [150,1]
+snake_position = [150,100]
 
 # fruit position
 fruit_position = [random.randrange(1, (screen_width//10)) * 10, 
                   random.randrange(1, (screen_height//10)) * 10]
 
 # snake_body
-snake_body = [[100,50]]
+snake_body = [[100,50],[90,50],[80,50]]
 
 #window size
 window_x = 720
@@ -34,49 +34,21 @@ game_window = pygame.display.set_mode((window_x, window_y))
 
 
 def show_score(choice, color, font, size):
-  
-    # creating font object score_font
     score_font = pygame.font.SysFont(font, size)
-    
-    # create the display surface object 
-    # score_surface
     score_surface = score_font.render('Score : ' + str(score), True, color)
-    
-    # create a rectangular object for the text
-    # surface object
-    score_rect = score_surface.get_rect()
-    
-    # displaying text
+    score_rect = score_surface.get_rect()    
     game_window.blit(score_surface, score_rect)
 
 def game_over():
-  
-    # creating font object my_font
-    my_font = pygame.font.SysFont('times new roman', 50)
-    
-    # creating a text surface on which text 
-    # will be drawn
+    my_font = pygame.font.SysFont('times new roman', 50)    
     game_over_surface = my_font.render(
         'Your Score is : ' + str(score), True, (255, 0, 0))
-    
-    # create a rectangular object for the text 
-    # surface object
     game_over_rect = game_over_surface.get_rect()
-    
-    # setting position of the text
     game_over_rect.midtop = (window_x/2, window_y/4)
-    
-    # blit will draw the text on screen
     game_window.blit(game_over_surface, game_over_rect)
     pygame.display.flip()
-    
-    # after 2 seconds we will quit the program
     time.sleep(2)
-    
-    # deactivating pygame library
     pygame.quit()
-    
-    # quit the program
     quit()
 
 
@@ -85,20 +57,24 @@ while True:
         if event.type == pygame.QUIT:
             sys.exit()
             
-    pygame.display.flip()
     pygame.display.update()
     clock.tick(60)  
     
     for pos in snake_body:
         pygame.draw.rect(screen, (0, 255, 0), pygame.Rect(pos[0], pos[1], 10, 10))
 
-    # # Detect collision with the wall
-    # if snake_position[0] < 0 or snake_position[0] > window_x-10:
-    #     game_over()
-    # if snake_position[1] < 0 or snake_position[1] > window_y-10:
-    #     game_over()
+    pygame.draw.rect(screen, (255, 0, 0), pygame.Rect(fruit_position[0], fruit_position[1], 10, 10))
 
-    # # Touching the snake body
-    # for block in snake_body[1:]:
-    #     if snake_position[0] == block[0] and snake_position[1] == block[1]:
-    #         game_over()
+    # Detect collision with the wall
+    if snake_position[0] < 0 or snake_position[0] > window_x-10:
+        game_over()
+    if snake_position[1] < 0 or snake_position[1] > window_y-10:
+        game_over()
+
+    # Touching the snake body
+    for block in snake_body[1:]:
+        if snake_position[0] == block[0] and snake_position[1] == block[1]:
+            game_over()
+
+    # displaying score continuously
+    show_score(1, (255, 255 ,255), 'times new roman', 20)
